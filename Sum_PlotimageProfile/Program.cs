f72e3ee35dc8ad96c9fc89a55eb34d27d66909b9
Georgi Danovski <georgi_danovski@abv.bg>
2020-04-08 17:03:36 +0300

    Add sec hit and ref index

--- Sum_PlotimageProfile/Program.cs
+++ Sum_PlotimageProfile/Program.cs
@@ -12,6 +12,7 @@ namespace Sum_PlotimageProfile
         static void Main(string[] args)
         {
             string dir;
+            int RefIndex = 0;
             Dictionary<string, Data> inputFiles = new Dictionary<string, Data>();
             do
             {
@@ -21,11 +22,21 @@ namespace Sum_PlotimageProfile
             }
             while (!Directory.Exists(dir));
 
+            do
+            {
+                Console.WriteLine("Ref index:");
+            }
+            while (!int.TryParse(Console.ReadLine(), out RefIndex));
+
             GetAllFiles(".txt",dir,inputFiles);
-            ProcessFiles(inputFiles);
+            ProcessFiles(inputFiles,RefIndex);
             string[] results = CalculateResults(inputFiles);
 
-            File.WriteAllLines(dir + "results.txt", results);
+            File.WriteAllLines(dir + "results_FirsFocus.txt", results);
+
+            results = CalculateResultsSecondFocus(inputFiles);
+            File.WriteAllLines(dir + "results_SecondFocus.txt", results);
+
             Console.WriteLine("Done!");
 
             Console.ReadLine();
@@ -33,17 +44,17 @@ namespace Sum_PlotimageProfile
         private static void GetAllFiles(string ext, string dir, Dictionary<string, Data> inputFiles)
         {
             foreach (string file in Directory.GetFiles(dir))
-                if (file.EndsWith(ext))
+                if (file.EndsWith(ext) && !file.EndsWith("Focus.txt"))
                     inputFiles.Add(file, new Data());
 
             foreach (string folder in Directory.GetDirectories(dir))
                 GetAllFiles(ext, folder, inputFiles);
         }
-        private static void ProcessFiles(Dictionary<string, Data> inputFiles)
+        private static void ProcessFiles(Dictionary<string, Data> inputFiles, int Refindex)
         {
             Parallel.ForEach(inputFiles, (file) => {
                 file.Value.FileName = Path.GetFileNameWithoutExtension(file.Key);                
-                file.Value.SetInputValues(File.ReadAllLines(file.Key));
+                file.Value.SetInputValues(File.ReadAllLines(file.Key),Refindex);
             });
         }
         private static string[] CalculateResults(Dictionary<string, Data> inputFiles)
@@ -53,7 +64,7 @@ namespace Sum_PlotimageProfile
 
             int column, row;
             List<string> cur = new List<string>();
-
+            cur.Add("Index");
 
             for (column = 0; column < inputFiles.ElementAt(0).Value.GetColumns; column++)
             {
@@ -66,16 +77,16 @@ namespace Sum_PlotimageProfile
 
             output[0] = string.Join("\t", cur);
             double val;
-
-
-
+            string ChartRange = "$A:$A";
+            bool addToExcell = true;
             for (row = 0; row < MaxRows; row++)
             {
-                int first = 0;
-                int last = inputFiles.Count - 1;
+                int first = 1;
+                int last = inputFiles.Count;
                 int totalLength = last + 2;
 
                 cur.Clear();
+                cur.Add(row.ToString());
 
                 for (column = 0; column < inputFiles.ElementAt(0).Value.GetColumns; column++)
                 {
@@ -86,20 +97,104 @@ namespace Sum_PlotimageProfile
                     }
 
                     string Avg = GetExcelCommand("AVERAGE", first, last, row);
+                    if(addToExcell)
+                        ChartRange += ",$" + GetColumnName(last + 1) + ":$" + GetColumnName(last + 1);
                     string StDev = GetExcelCommand("STDEV.S", first, last, row);
 
-                    first += totalLength + 1;
-                    last += totalLength + 1;
+                    first += totalLength;
+                    last += totalLength;
 
                     cur.Add(Avg);
                     cur.Add(StDev);
 
                 }
-
+                addToExcell = false;
                 output[row + 1] = string.Join("\t", cur);
             }
+            ChartRange = ChartRange.Substring(1);
 
+           // Console.WriteLine(GetMacro(ChartRange));
             return output;
+        }
+        private static string[] CalculateResultsSecondFocus(Dictionary<string, Data> inputFiles)
+        {
+            int MaxRows = FindMaxRows(inputFiles);
+            string[] output = new string[MaxRows + 1];
+
+            int column, row;
+            List<string> cur = new List<string>();
+            cur.Add("Index");
+            for (column = 0; column < inputFiles.ElementAt(0).Value.GetColumns; column++)
+            {
+                foreach (var kvp in inputFiles)
+                    cur.Add(kvp.Value.GetTitle(column) + "_" + kvp.Value.FileName);
+
+                cur.Add("Avg_" + inputFiles.ElementAt(0).Value.GetTitle(column));
+                cur.Add("StDev_" + inputFiles.ElementAt(0).Value.GetTitle(column));
+            }
+
+            output[0] = string.Join("\t", cur);
+            double val;
+            string ChartRange = "$A:$A";
+            bool addToExcell = true;
+            for (row = 0; row < MaxRows; row++)
+            {
+                int first = 1;
+                int last = inputFiles.Count;
+                int totalLength = last + 2;
+
+                cur.Clear();
+                cur.Add(row.ToString());
+
+                for (column = 0; column < inputFiles.ElementAt(0).Value.GetColumns; column++)
+                {
+                    foreach (var kvp in inputFiles)
+                    {
+                        val = kvp.Value.GetValueSecondFocus(column, row);
+                        cur.Add(val != 0 ? val.ToString() : "");
+                    }
+
+                    string Avg = GetExcelCommand("AVERAGE", first, last, row);
+                    string StDev = GetExcelCommand("STDEV.S", first, last, row);
+                    if(addToExcell)
+                        ChartRange += ",$" + GetColumnName(last + 1) + ":$" + GetColumnName(last + 1);
+                    first += totalLength;
+                    last += totalLength;
+
+                    cur.Add(Avg);
+                    cur.Add(StDev);
+
+                }
+                addToExcell = false;
+                output[row + 1] = string.Join("\t", cur);
+            }
+            ChartRange = ChartRange.Substring(1);
+            Console.WriteLine(GetMacro(ChartRange));
+            return output;
+        }
+        private static string GetMacro(string range)
+        {
+            return (@"
+-----------------------------------------
+Excel macro
+-----------------------------------------
+
+    Range('" + range.Replace("$","") + @"').Select
+    
+    With Selection.Interior
+        .Pattern = xlSolid
+        .PatternColorIndex = xlAutomatic
+        .ThemeColor = xlThemeColorAccent2
+        .TintAndShade = 0.599993896298105
+        .PatternTintAndShade = 0
+    End With
+    
+    ActiveSheet.Shapes.AddChart2(240, xlXYScatterSmoothNoMarkers).Select
+    ActiveChart.SetSourceData Source:=Range('" + range + @"')
+
+-----------------------------------------
+
+").Replace("'","\"");
         }
         private static int FindMaxRows(Dictionary<string, Data> inputFiles)
         {
@@ -132,13 +227,16 @@ namespace Sum_PlotimageProfile
 
             return name;
         }
+        
     }
     class Data
     {
         private string fileName;
-        private int maxIndex = 0;
+        //private int maxIndex = 0;
         private string[] titles;
         private double[,] values;//[column,row]
+        private double[,] valuesSecFocus;//[column,row]
+
         public string FileName
         {
             set
@@ -171,16 +269,23 @@ namespace Sum_PlotimageProfile
             else
                 return 0d;
         }
+        public double GetValueSecondFocus(int column, int row)
+        {
+            if (column < GetColumns && row < GetRows)
+                return valuesSecFocus[column, row];
+            else
+                return 0d;
+        }
         public string GetTitle(int column)
         {
             return titles[column];
         }
-        public void SetInputValues(string[] input)
+        public void SetInputValues(string[] input, int RefIndex)
         {
             if (input == null || input.Length < 2) return;
 
             double MaxValue = double.MinValue;
-            
+           int maxIndex = 0;
 
             titles = input[0].Split(new string[] { "\t" }, StringSplitOptions.None);
 
@@ -194,30 +299,65 @@ namespace Sum_PlotimageProfile
                 for (int column = 0; column < GetColumns; column++)
                     double.TryParse(rowString[column], out newValues[column, row]);
 
-                if(newValues[5, row] > MaxValue)
+                if(newValues[RefIndex, row] > MaxValue)
                 {
-                    MaxValue = newValues[5, row];
-                    this.maxIndex = row;
+                    MaxValue = newValues[RefIndex, row];
+                    maxIndex = row;
                 }
             }
             //recalculate matrix
             if (maxIndex < (double)newValues.GetLength(1) / 2d)//copy with new index
             {
-                this.values = new double[GetColumns, newValues.GetLength(1) - this.maxIndex];
+                this.values = new double[GetColumns, newValues.GetLength(1) - maxIndex];
 
-                for (int row = this.maxIndex, newRow = 0; row < newValues.GetLength(1); row++,newRow++)
+                for (int row = maxIndex, newRow = 0; row < newValues.GetLength(1); row++,newRow++)
                     for (int column = 0; column < GetColumns; column++)
                         this.values[column, newRow] = newValues[column, row];
             }
             else//revert and copy
             {
-                this.values = new double[GetColumns, this.maxIndex + 1];
+                this.values = new double[GetColumns, maxIndex + 1];
 
-                for (int row = this.maxIndex, newRow = 0; row >= 0; row--, newRow++)
+                for (int row = maxIndex, newRow = 0; row >= 0; row--, newRow++)
                     for (int column = 0; column < GetColumns; column++)
                         this.values[column, newRow] = newValues[column, row];
             }
 
+            CalculateSecFoci(RefIndex);
+        }
+        public void CalculateSecFoci(int RefIndex)
+        {
+            int col, row, newRow, Half,maxIndex;
+            double Max;
+            this.valuesSecFocus = new double[GetColumns, GetRows];
+
+            //find half
+            col = GetColumns / 2 + RefIndex;
+            if (col >= GetColumns) col = GetColumns - 1;
+            Half = 0;
+            for (row = GetRows - 1; row > 0; row--)
+                if (GetValue(col, row) != 0d)
+                {
+                    Half = (int)(row / 2);
+                    break;
+                }
+
+            for (col = 0; col < GetColumns; col++)
+            {                
+                //find max index
+                Max = double.MinValue;
+                maxIndex = 0;
+                for (row = Half;row<GetRows;row++)
+                    if(GetValue(col, row) > Max)
+                    {
+                        Max = GetValue(col, row);
+                        maxIndex = row;
+                    }
+                //copy reversed
+                
+                for (row = maxIndex, newRow = 0; row >= 0; row--, newRow++)
+                        this.valuesSecFocus[col, newRow] = this.values[col, row];
+            }
         }
     }
 }
